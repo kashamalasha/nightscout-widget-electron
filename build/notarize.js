3ce080dac8366515bebe5661493fb30a5cca4fa7
kashamalasha <dmitry.burnyshev@gmail.com>
2023-08-10 19:57:14 +0200

    10.08

--- build/notarize.js
+++ build/notarize.js
@@ -1,58 +1,54 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 const electronNotarize = require('@electron/notarize');
 
 module.exports = async function (context) {
+  let params = {
+    tool: `notarytool`,
+    appPath: path.join(context.appOutDir, `${context.packager.appInfo.productFilename}.app`),
+  };
 
-    const params = {
-        tool: `notarytool`,
-        appPath: path.join(context.appOutDir, `${context.packager.appInfo.productFilename}.app`)
+  if (!process.env.GITHUB_ACTIONS) {
+    const dotenv = require('dotenv');
+    const result = dotenv.config({ path: path.join(__dirname, '.env') });
+
+    params = {
+      ...params,
+      appBundleId: process.env.APP_ID,
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID,
     };
 
-    if (!process.env.GITHUB_ACTIONS) {
-        const dotenv = require(`dotenv`)
-        const result = dotenv.config({ path: path.join(__dirname, `.env`)})
-
-        params = { 
-        ...params, 
-        appBundleId: process.env.APP_ID,
-        appleId: process.env.APPLE_ID,
-        appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
-        teamId: process.env.APPLE_TEAM_ID,
-        }
-
-        if (result.error) {
-            throw result.error
-        }
-    } else {
-        params = {
-        ... params,
-        keychain: process.env.KEYCHAIN,
-        keychainProfile: process.env.KEYCHAIN_PROFILE,
-        }
+    if (result.error) {
+      throw result.error;
     }
+  } else {
+    params = {
+      ...params,
+      keychain: process.env.KEYCHAIN,
+      keychainProfile: process.env.KEYCHAIN_PROFILE,
+    };
+  }
 
-    const prefix = `  •`
+  if (process.platform !== 'darwin') {
+    return;
+  }
 
-    if (process.platform !== 'darwin') {
-        return
-    }
-    console.log(context.packager.appInfo)
+  console.log(`${prefix || ''} afterSign hook triggered for the ${context.packager.appInfo.productFilename}.app`);
 
-    console.log(`${prefix} afterSign hook triggered for the ${context.packager.appInfo.productFilename}.app`)
+  if (!fs.existsSync(params.appPath)) {
+    console.log(`${prefix || ''} !! notarization process was skipped due to File not found`);
+    return;
+  }
 
-    if (!fs.existsSync(params.appPath)) {
-        console.log(`${prefix} !! notarization process was skiped due to File not found`)
-        return
-    }
+  console.log(`${prefix || ''} notarizing App file at ${params.appPath}`);
 
-    console.log(`${prefix} notarizing App file at ${params.appPath}`)
+  try {
+    await electronNotarize.notarize(params);
+  } catch (error) {
+    console.error(error);
+  }
 
-    try {
-        await electronNotarize.notarize(params)
-    } catch (error) {
-        console.error(error)
-    }
-
-    console.log(`${prefix} done notarizing for the ${context.packager.appInfo.productFilename}.app`)
-}
+  console.log(`${prefix || ''} done notarizing for the ${context.packager.appInfo.productFilename}.app`);
+};
