1d0bc2d8815a8bfb21607d5738c1b91504976dfd
kashamalasha <dmitry.burnyshev@gmail.com>
2025-01-27 21:50:31 +0200

    WIP: Add AutoUpdate enhancement

--- js/auto-update.js
+++ js/auto-update.js
@@ -1,5 +1,7 @@
 const { autoUpdater } = require(`electron-updater`);
-const { Store } = require(`electron-store`);
+const { ProgressBar } = require(`electron-progressbar`);
+const { dialog } = require(`electron`);
+const Store = require(`electron-store`);
 const log = require(`./logger`);
 
 const config = new Store();
@@ -22,7 +24,32 @@ const requestToUpdate = () => {
     log.info(`Update was already requested for today`);
     return;
   }
-  autoUpdater.checkForUpdatesAndNotify();
+  // autoUpdater.checkForUpdatesAndNotify();
+  autoUpdater.checkForUpdates();
+  autoUpdater.on(`update-available`, () => {
+    const response = dialog.showOpenDialogSync({
+      // TODO: implement the dialog for updates
+    });
+  });
+  autoUpdater.on(`download-progress`, (progress) => {
+    const progressBar = new ProgressBar({
+      text: `Downloading update...`,
+      detail: `Downloading ${progress.percent.toFixed(2)}%`,
+      icon: `asset/icons/png/128x128.png`
+    });
+
+    progressBar
+      .on('completed', () => {
+        console.info(`completed...`);
+        progressBar.detail = 'Task completed. Exiting...';
+      })
+      .on('aborted', (value) => {
+        console.info(`aborted... ${value}`);
+      })
+      .on('progress', (value) => {
+        progressBar.detail = `Value ${value} out of ${progressBar.getOptions().maxValue}...`;
+      });
+  });
 };
 
 module.exports = requestToUpdate;
